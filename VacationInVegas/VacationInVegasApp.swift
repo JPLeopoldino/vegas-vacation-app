91d107da8799ff999ceea7a7d9e3466f53e17f19
João Pedro Leopoldino <leopoldinodev@gmail.com>
2024-11-15 15:29:28 -0300

    Initial Commit

---
+++ VacationInVegas/VacationInVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasApp.swift
+//  VacationInVegas
+//
+//  Created by João Pedro Leopoldino on 2024-11-15.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
